a409073e9bf4a46f9572c037adbf85a692be0b8b
Michal Halabica <m.halabica@gmail.com>
2020-03-21 15:23:54 +0100

    Prvotní implementace načítání parametrů.

---
+++ test/arg_parser.php
@@ -0,0 +1,78 @@
+<?php
+class CommandLineArguments
+{
+    public $help = false;
+    public $directory = '';
+    public $recursive = false;
+    public $parseScript = 'parse.php';
+    public $intScript = 'interpret.py';
+    public $parseOnly = false;
+    public $intOnly = false;
+    public $jexamxml = '/pub/courses/ipp/jexamxml/jexamxml.jar';
+
+    public function __construct()
+    {
+        $this->directory = getcwd();
+    }
+
+    public function runAll()
+    {
+        return !$this->parseOnly && !$this->intOnly && !$this->help;
+    }
+}
+
+class CommandLineArgsParseService
+{
+    public function parse($argc, $argv)
+    {
+        if ($argc == 1)
+            return new CommandLineArguments();
+
+        try {
+            return $this->parseArguments($argv);
+        } catch (ErrorException $e) {
+            // TODO Zabíjení aplikace, když nastane chyba.
+        }
+    }
+
+    /**
+     * Nacitani parametru prikazove radky.
+     *
+     * @param mixed $argv
+     * @return CommandLineArguments
+     */
+    private function parseArguments($argv)
+    {
+        $args = new CommandLineArguments();
+
+        // TODO: Kombinování parametrů
+        // TODO: Duplicitní parametry.
+
+        foreach ($argv as $arg) {
+            $this->trySetValue($arg, 'help', $args->help);
+            $this->trySetValue($arg, 'recursive', $args->recursive);
+            $this->trySetValue($arg, 'parse-only', $args->parseOnly);
+            $this->trySetValue($arg, 'int-only', $args->intOnly);
+            $this->trySetPath($arg, 'directory', $args->directory);
+            $this->trySetPath($arg, 'parse-script', $args->parseScript);
+            $this->trySetPath($arg, 'int-script', $args->intScript);
+            $this->trySetPath($arg, 'jexamxml', $args->jexamxml);
+        }
+
+        return $args;
+    }
+
+    private function trySetValue($currentVal, $key, &$output)
+    {
+        if ($currentVal == "--$key") {
+            $output = true;
+        }
+    }
+
+    private function trySetPath($currentVal, $key, &$path)
+    {
+        if (preg_match("/--$key=(.+)/", $currentVal, $matches)) {
+            $path = $matches[1];
+        }
+    }
+}
--- test/test.php
+++ test/test.php
@@ -1,9 +1,21 @@
 <?php
+include './arg_parser.php';
 
 class Program
 {
+    private $commandLineParser;
+
+    public function __construct()
+    {
+        $this->commandLineParser = new CommandLineArgsParseService();
+    }
+
     public function Run($argc, $argv)
     {
+        $args = $this->commandLineParser->parse($argc, $argv);
+
+        var_dump($args);
+        echo "RunAll: ".$args->runAll();
     }
 }
 
